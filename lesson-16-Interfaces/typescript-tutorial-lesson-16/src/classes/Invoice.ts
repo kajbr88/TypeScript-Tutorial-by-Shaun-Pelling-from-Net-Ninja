989ee48171c00476ecf53193a6a1c7325e1a6e87
A K B <Abhijeet Kumar Barua>
2025-04-15 01:11:54 +0530

    Initial commit

---
+++ lesson-16-Interfaces/typescript-tutorial-lesson-16/src/classes/Invoice.ts
@@ -0,0 +1,16 @@
+import { HasFormatter } from '../interfaces/HasFormatter.js';
+
+export class Invoice implements HasFormatter {
+  constructor(
+    readonly client: string, 
+    private details: string, 
+    public amount: number,
+  ){}
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+  format2() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
